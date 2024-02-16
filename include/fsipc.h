fd4b1b77afb7570b7c70cdf3f4c4721897f33df2
HHgzs <3166237657@qq.com>
2024-02-16 20:07:46 +0800

    free方法更新

--- include/fsipc.h
+++ include/fsipc.h
@@ -3,7 +3,11 @@
 #include <string>
 
 namespace fsipc {
-    key_t __gen_hash_shm_key__(const std::string& __str);
+
+    key_t __shm_gen_hash_key__(const std::string& __str);
+    void* __shm_alloc__(key_t __key__, size_t __size__);
+    void __shm_free__(key_t __key__);
+
 }
 
 
--- src/fsipc.cpp
+++ src/fsipc.cpp
@@ -1,15 +1,35 @@
 #include "fsipc.h"
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string>
 
-key_t fsipc::__gen_hash_shm_key__(const std::string& __str__) {
+key_t fsipc::__shm_gen_hash_key__(const std::string& __str__) {
     unsigned long __hash = 5381;
     const char* __cstr = __str__.c_str();
     int __c;
     while ((__c = *__cstr++))
         __hash = ((__hash << 5) + __hash) + __c;
-
     return (key_t)__hash;
 }
 
+void* fsipc::__shm_alloc__(key_t __key__, size_t __size__) {
+    int __shmid = shmget(__key__, __size__, IPC_CREAT | IPC_EXCL | 0666);
+    if (__shmid == -1) {
+        __shmid = shmget(__key__, 0, 0);
+        if (__shmid == -1) { perror("shmget"); return NULL; }
+    }
+    void* __shm_ptr = shmat(__shmid, NULL, 0);
+    if (__shm_ptr == (void*)-1) { perror("shmat"); return NULL; }
+
+    return __shm_ptr;
+}
+
+void fsipc::__shm_free__(key_t __key__) {
+    int shmid = shmget(__key__, 0, 0);
+    if (shmid == -1) { perror("shmget"); return; }
+    if (shmctl(shmid, IPC_RMID, NULL) == -1) { perror("shmctl"); return; }
+}
